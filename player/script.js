36529a25e8793b7857c188671eac4abaa27df4e4
dcvslab <dcvslab@users.noreply.github.com>
2015-09-19 22:03:09 -0400

    Update script.js

--- player/script.js
+++ player/script.js
@@ -4,13 +4,24 @@ function mAdd() {
       if (! mlink.split("youtu.be")[1]) {
         if (! mlink.split("soundcloud.com")[1]) {
           alert("This is not a valid link!")
-        } else { //soundcloud
-            $.get('https://api.soundcloud.com/resolve.json?url=' + mlink + '&client_id=bb52b4a0c199ecc98cd5d4e4ddf2ee14', 
-              function (scdata) {
-                if (scdata.kind != "track") { alert("This is not a valid link!")} 
-                else { alert("This is a valid link!")}
+        } else { 
+          if (! mlink.split("soundcloud.com")[1]) {
+            
+          } else { //soundcloud
+            var scpl = mlink.split("soundcloud.com/")[1]
+            var sclink = "https://www.soundcloud.com/" + scpl
+            $.ajax({
+              url: "https://api.soundcloud.com/resolve.json?url=" + sclink + "&client_id=bb52b4a0c199ecc98cd5d4e4ddf2ee14", 
+              dataType: "jsonp",
+              success: function(data){
+               console.log(data);           
+              },
+              error: function(jqXHR, textStatus, errorThrown) {
+                alert("This is not a valid link!");
+              }
             });
 
+          }
         }
       }
     }
